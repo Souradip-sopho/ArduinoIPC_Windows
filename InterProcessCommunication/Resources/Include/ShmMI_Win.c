a6eadf6d705f58b3b5bb1831d0f8c0bb30799f5b
Souradip <20797533+Souradip-sopho@users.noreply.github.com>
2019-06-17 13:18:41 +0530

    Modified SHM for IPC modelica. Used two filemappings for Read and Write. Tested and working.

--- InterProcessCommunication/Resources/Include/ShmMI_Win.c
+++ InterProcessCommunication/Resources/Include/ShmMI_Win.c
@@ -1,158 +1,158 @@
-#include <windows.h>
+#include <Windows.h>
 #include <stdio.h>
 #include <conio.h>
 #include <tchar.h>
-#include "SerialMI_Win.h"
-
-
-// struct DataExchange {
-//     char sendVal[10][32];
-//     double getVal[10];
-// };
-
-//struct DataExchange *PID;
-
-static HANDLE hMapFile;
+#pragma comment(lib, "user32.lib")
 
 #define BUF_SIZE 256
 
-TCHAR szName[]=TEXT("Global\\MyFileMappingObject");
-TCHAR szMsg[]=TEXT("Message from first process.");
+TCHAR szName1[]=TEXT("Global\\MyFileMappingObject1");
+TCHAR szName2[]=TEXT("Global\\MyFileMappingObject2");
+
+HANDLE hMapFile1;
+HANDLE hMapFile2;
 
 __declspec(dllexport)  __stdcall double shmWrite(int num1,double tagValue)
 {
 
-   printf("Starting Shared Memory Write..\n");
-   //LPCTSTR pBuf;
-   char* pBuf;
+	printf("Starting Shared Memory Write..\n");
+	// LPCTSTR pBuf;
+	char* pBuf;
 
-   hMapFile = CreateFileMappingA(
-                 INVALID_HANDLE_VALUE,    // use paging file
-                 NULL,                    // default security
-                 PAGE_READWRITE,          // read/write access
-                 0,                       // maximum object size (high-order DWORD)
-                 BUF_SIZE,                // maximum object size (low-order DWORD)
-                 szName);                 // name of mapping object
+	hMapFile2 = CreateFileMappingA(
+						INVALID_HANDLE_VALUE,    // use paging file
+						NULL,                    // default security
+						PAGE_READWRITE,          // read/write access
+						0,                       // maximum object size (high-order DWORD)
+						BUF_SIZE,                // maximum object size (low-order DWORD)
+						szName2);                 // name of mapping object
 
-   if (GetLastError() == ERROR_ALREADY_EXISTS) {
-        hMapFile = OpenFileMappingA(FILE_MAP_ALL_ACCESS, FALSE, szName);
-   }
+	if (GetLastError() == ERROR_ALREADY_EXISTS) {
+	    hMapFile2 = OpenFileMappingA(FILE_MAP_ALL_ACCESS, FALSE, szName2);
+	}
 
-   if (hMapFile == NULL)
-   {
-      _tprintf(TEXT("Could not create file mapping object (%d).\n"),
-             GetLastError());
-	     return 1;
-   }
-   
-   /*pBuf = (LPTSTR) MapViewOfFile(hMapFile,   // handle to map object
-                        FILE_MAP_ALL_ACCESS, // read/write permission
-                        0,
-                        0,
-                        BUF_SIZE);
-    */
-    pBuf = (char*) MapViewOfFile(hMapFile,   // handle to map object
-                        FILE_MAP_ALL_ACCESS, // read/write permission
-                        0,
-                        0,
-                        BUF_SIZE);
+	if (hMapFile2 == NULL)
+	{
+		_tprintf(TEXT("Could not create file mapping object (%d).\n"),
+		    GetLastError());
+		return 1;
+	}
 
-   if (pBuf == NULL)
-   {
-      _tprintf(TEXT("Could not map view of file (%d).\n"),
-             GetLastError());
+	/*pBuf = (LPTSTR) MapViewOfFile(hMapFile,   // handle to map object
+	                    FILE_MAP_ALL_ACCESS, // read/write permission
+	                    0,
+	                    0,
+	                    BUF_SIZE);
+	*/
+	pBuf = (char*) MapViewOfFile(hMapFile2,   // handle to map object
+	                    FILE_MAP_ALL_ACCESS, // read/write permission
+	                    0,
+	                    0,
+	                    BUF_SIZE);
 
-       CloseHandle(hMapFile);
-       return 1;
-   }
-   
-   char outData[256]="";
-   sprintf(outData,"%d,%g\n", num1, tagValue);
-   printf("OutData:%s",outData);
-   /*TCHAR outData[]=TEXT("");
-   _stprintf(outData,"%d,%g\n",num1,tagValue);
-   _tprintf("%s\n",outData);
-    */
+	if (pBuf == NULL)
+	{
+		_tprintf(TEXT("Could not map view of file (%d).\n"),
+		    GetLastError());
 
-   memcpy(pBuf,outData,BUF_SIZE);
-   //CopyMemory((PVOID)pBuf, outData, (_tcslen(outData) * sizeof(TCHAR)));
-   //CopyMemory((PVOID)pBuf, outData, strlen(outData)*sizeof(char));
-   //_getch();
-   FlushViewOfFile (hMapFile, BUF_SIZE);
-   UnmapViewOfFile(pBuf);
+		CloseHandle(hMapFile2);
+		return 1;
+	}
 
-   //CloseHandle(hMapFile);
-   return 0;
+	char outData[BUF_SIZE]="";
+	sprintf(outData,"%d,%g\n", num1, tagValue);
+	printf("OutData:%s",outData);
+
+	/*TCHAR outData[]=TEXT("");
+	_stprintf(outData,"%d,%g\n",num1,tagValue);
+	_tprintf("%s\n",outData);
+	*/
+
+	memcpy(pBuf,outData,BUF_SIZE);
+	//CopyMemory((PVOID)pBuf, outData, (_tcslen(outData) * sizeof(TCHAR)));
+	//CopyMemory((PVOID)pBuf, outData, strlen(outData)*sizeof(char));
+	//_getch();
+	//FlushViewOfFile (hMapFile, BUF_SIZE);
+	
+	UnmapViewOfFile(pBuf);
+	//CloseHandle(hMapFile);
+
+	return 0;
 }
 
 
 __declspec(dllexport)  __stdcall double shmRead(int num2)
 {
-   //LPCTSTR pBuf;
-   char* pBuf;
-   printf("Staring Shared Memory Read...\n");
-   hMapFile = OpenFileMappingA(
-                   FILE_MAP_ALL_ACCESS,   // read/write access
-                   FALSE,                 // do not inherit the name
-                   szName);               // name of mapping object
+	printf("Starting Shared Memory Read...\n");
+	// LPCTSTR pBuf;
+	char* pBuf;
 
-   if (hMapFile == NULL)
-   {
-      _tprintf(TEXT("Could not open file mapping object (%d).\n"),
-             GetLastError());
-	     return 0;
-   }
+	hMapFile1 = OpenFileMappingA(
+	               FILE_MAP_ALL_ACCESS,   // read/write access
+	               FALSE,                 // do not inherit the name
+	               szName1);               // name of mapping object
 
-   /*pBuf = (LPTSTR) MapViewOfFile(hMapFile, // handle to map object
-               FILE_MAP_ALL_ACCESS,  // read/write permission
-               0,
-               0,
-               BUF_SIZE);
-    */
-   pBuf = (char*) MapViewOfFile(hMapFile, // handle to map object
-               FILE_MAP_ALL_ACCESS,  // read/write permission
-               0,
-               0,
-               BUF_SIZE);
+	if (hMapFile1 == NULL)
+	{
+		_tprintf(TEXT("Could not open file mapping object (%d).\n"),
+		    GetLastError());
+		 return 0;
+	}
 
-   if (pBuf == NULL)
-   {
-      _tprintf(TEXT("Could not map view of file (%d).\n"),
-             GetLastError());
+	/*pBuf = (LPTSTR) MapViewOfFile(hMapFile, // handle to map object
+						FILE_MAP_ALL_ACCESS,  // read/write permission
+						0,
+						0,
+						BUF_SIZE);
+	*/
+	pBuf = (char*) MapViewOfFile(hMapFile1, // handle to map object
+						FILE_MAP_ALL_ACCESS,  // read/write permission
+						0,
+						0,
+						BUF_SIZE);
 
-      CloseHandle(hMapFile);
-      return 0;
-   }
+	if (pBuf == NULL)
+	{
+		_tprintf(TEXT("Could not map view of file (%d).\n"),
+		    GetLastError());
 
-   //printf("p:%s",pBuf);
-   //MessageBox(NULL, pBuf, TEXT("Process2"), MB_OK);
+		CloseHandle(hMapFile1);
+		return 0;
+	}
 
-   char inData[256];
-   //memcpy(inData, pBuf, _tcslen(pBuf));
-   memcpy(inData,pBuf,BUF_SIZE);
-   printf("InData:%s",inData);
-	 int adr;	
-   float returnVal=0;
-   if(strcmp(inData,"")!=0)
-   {
-     if(sscanf(inData,"%d,%g\n",&adr,&returnVal)==0)
-     {
-  	    _tprintf(TEXT("Error reading shared memory\n"));
-        UnmapViewOfFile(pBuf);
-        CloseHandle(hMapFile);
-     }
-     if(adr!=num2)
-      {
-        _tprintf(TEXT("Error accessing shared memory\n"));
-        UnmapViewOfFile(pBuf);
-        CloseHandle(hMapFile);
-      }
-      
-  	 printf("OutputValue:%g\n",returnVal);
-   }
-   UnmapViewOfFile(pBuf);
+	//printf("p:%s",pBuf);
+	//MessageBox(NULL, pBuf, TEXT("Process2"), MB_OK);
 
-   CloseHandle(hMapFile);
-   
-  return returnVal;
+	char inData[BUF_SIZE]="";
+	//memcpy(inData, pBuf, _tcslen(pBuf));
+	memcpy(inData,pBuf,BUF_SIZE);
+	printf("InData:%s",inData);
+
+	int adr;	
+	float returnVal=0;
+
+	if(strcmp(inData,"")!=0)
+	{
+		if(sscanf(inData,"%d,%g\n",&adr,&returnVal)==0)
+		{
+		    _tprintf(TEXT("Error reading shared memory\n"));
+			UnmapViewOfFile(pBuf);
+			// CloseHandle(hMapFile);
+		}
+		if(adr!=num2)
+		{
+			_tprintf(TEXT("Error accessing shared memory\n"));
+			UnmapViewOfFile(pBuf);
+			// CloseHandle(hMapFile);
+		}
+
+		printf("OutputValue: %d:%g\n",adr,returnVal);
+	}
+
+	UnmapViewOfFile(pBuf);
+	//CloseHandle(hMapFile);
+
+	return returnVal;
 }
+
+
